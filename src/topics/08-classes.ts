06c425203b057a4665066e6d8ac97caa0cadfac8
SaintNox <santosirving36@gmail.com>
2025-05-19 15:18:45 -0700

    08-classes

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -3,9 +3,46 @@ export class Person {
     // public address: string;
 //FORMA CORTA =
     constructor(// se pueden definir los tipos de datos de los argumentos unicamnete asignandole el nivel de acceso a cada variable y automaticamente se crean las propiedades y se asignan los valores
-        public name: string,
-        public address: string) {} 
+        public firstName: string,
+        public lastName: string,
+        public address: string= 'No address') {} 
+}//extender una clase es la expansion de su funcionalidad
+
+
+// export class Hero extends Person { //cuando se defina en la clase Hero, se aplica a Hero y se extiende a Person
+
+
+    
+
+//     constructor(
+//         public alertEgo: string,
+//         public age: number,
+//         public realName: string
+//     ){
+//         super(realName, 'New york');//super es una funcion especial que le indica a typescript que tiene que llamar al constructor del padre
+//     }
+// }
+
+export class Hero  { //cuando se defina en la clase Hero, se aplica a Hero y se extiende a Person
+
+
+    // public person: Person;
+
+    
+
+    constructor(
+        public alertEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person,
+
+    ){
+        // this.person = new Person(realName);
+    }
+
 }
 
-const ironman = new Person('Ironman', 'new York');// person "()" los parentesis son el llamado a una funcion que le mande argumentos
+const tony = new Person('Tony stark','stark', 'New York');
+
+const ironman = new Hero('Ironman', 45, 'Tony',tony);// person "()" los parentesis son el llamado a una funcion que le mande argumentos
 console.log(ironman);
